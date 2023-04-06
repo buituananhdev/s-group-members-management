fbec2a2fac3cb8f3998c4be0f7036c486eb3bbe8
TuanAnhDepTrai <anhaanh2003@gmail.com>
2023-04-07 00:54:28 +0700

    done

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const app = express();
 // Require user route
 const userRoutes = require('./routes/user');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // Dùng userRoute cho tất cả các route bắt đầu bằng '/users'
 app.use('/users', userRoutes);
 // ... Other routes
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 app.listen(3000, () => console.log('Server started on port 3000'));
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const user_router = express.Router();
 
-
 const fs = require('fs');
 const path = require('path');
 // get the path to the users.json file
+// eslint-disable-next-line no-undef
 const usersFilePath = path.join(__dirname, '../data/users.json');
+console.log(usersFilePath)
 
-// get the initial users from the users.json file
 let arr = [];
 if (fs.existsSync(usersFilePath)) {
     arr = JSON.parse(fs.readFileSync(usersFilePath));
@@ -15,9 +15,7 @@ if (fs.existsSync(usersFilePath)) {
 
 // get all users
 user_router.get('', (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-    res.status(200).send(arr);
+    return res.status(200).json(arr);
 })
 
 // get information user by id
@@ -25,19 +23,24 @@ user_router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const userFind = arr.find((user) => user.id === id);
     if (userFind) {
-        res.status(200).send(userFind);
+        return res.status(200).json(userFind);
     } else {
-        res.status(400).send("User not found!");
+        return res.status(400).json("User not found!");
     }
 })
 
 // add new user
 user_router.post('', (req, res) => {
-    let newUser = Object.assign({id: arr.length + 1}, {...req.body});
+    let newUser = {
+        id: arr.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    };
     arr.push(newUser);
     // write the updated users array to the users.json file
     fs.writeFileSync(usersFilePath, JSON.stringify(arr));
-    res.status(201).send(arr[arr.length -1 ]);
+    return res.status(201).json(arr[arr.length -1 ]);
 })
 
 // delete user by id
