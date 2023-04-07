e2bcede56c5c8198f6bfe8162292db8a50e9259d
TuanAnhDepTrai <anhaanh2003@gmail.com>
2023-04-07 20:37:29 +0700

    done

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -2,7 +2,6 @@ const unidecode = require('unidecode');
 function validate(req, res, next){
     let str = req.body.fullname;
     str = unidecode(str);
-    console.log(str);
     for(let i = 0;i < str.length;i++){
         if(str[i] == ' ') continue;
         const ascii = str.charCodeAt(i);
@@ -10,7 +9,7 @@ function validate(req, res, next){
             return res.status(400).json('Ten khong duoc chua ki tu so va ki tu dac biet!');
         }
     }
-    if (req.body.age < 0) {
+    if (req.body.age <= 0) {
         return res.status(400).json('Tuoi phai lon hon 0!');
     }
     console.log(req.body)
