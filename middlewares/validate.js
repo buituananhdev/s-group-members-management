335da0203772e6685656c6d8a4a907ca7ea8d7ca
TuanAnhDepTrai <anhaanh2003@gmail.com>
2023-04-11 16:42:11 +0700

    add pro regex

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,18 +1,29 @@
-const unidecode = require('unidecode');
-function validate(req, res, next){
-    let str = req.body.fullname;
-    str = unidecode(str);
-    for(let i = 0;i < str.length;i++){
-        if(str[i] == ' ') continue;
-        const ascii = str.charCodeAt(i);
-        if((ascii < 65 || ascii > 90) && (ascii < 97 || ascii > 122)){
-            return res.status(400).json('Ten khong duoc chua ki tu so va ki tu dac biet!');
-        }
+// const unidecode = require('unidecode');
+// function validate(req, res, next){
+//     if(req.body.fullname.length == 0) {
+//         return res.status(400).json("Ten khong rong!");
+//     }
+//     let str = req.body.fullname;
+//     str = unidecode(str);
+//     for(let i = 0;i < str.length;i++){
+//         if(str[i] == ' ') continue;
+//         const ascii = str.charCodeAt(i);
+//         if((ascii < 65 || ascii > 90) && (ascii < 97 || ascii > 122)){
+//             return res.status(400).json('Ten khong duoc chua ki tu so va ki tu dac biet!');
+//         }
+//     }
+//     if (req.body.age <= 0) {
+//         return res.status(400).json('Tuoi phai lon hon 0!');
+//     }
+//     next();
+// }
+
+// BAO TEST :V
+function validate(req, res, next) {
+    const regex = /^[a-zA-Z\u00C0-\u024F\u1EA0-\u1EF9 ]+$/;
+    if (!regex.test(req.body.fullname)) {
+        return res.status(400).json('Ten khong duoc chua ki tu so va ki tu dac biet!');
     }
-    if (req.body.age <= 0) {
-        return res.status(400).json('Tuoi phai lon hon 0!');
-    }
-    console.log(req.body)
     next();
 }
 
