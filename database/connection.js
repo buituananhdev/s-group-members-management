cc949a2f98b29a739eae6b7957d7fed3aa06478f
TuanAnh1003 <anhaanh2003@gmail.com>
2023-05-30 20:48:04 +0700

    add knex

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,23 @@
-const mysql = require('mysql2');
+//const mysql = require('mysql2/promise');
 require('dotenv').config();
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    multipleStatements: false // chan query nhieu lenh
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     multipleStatements: false // chan query nhieu lenh
+// });
+
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
 });
-module.exports = connection;
+
+module.exports = knex;
--- routes/user.js
+++ routes/user.js
@@ -1,81 +1,98 @@
 const express = require('express');
 const user_router = express.Router();
+const jwt = require('jsonwebtoken');
 const { validate_name } = require('../middleware/validate');
 const authentication = require('../middleware/authentication');
 user_router.use(express.json());
 user_router.use(express.urlencoded({ extended: true }));
-const connection = require('../database/connection');
+const knex = require('../database/connection');
 // get all users
-user_router.get('', authentication, (req, res) => {
-    connection.query('SELECT * FROM Users', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        return res.json(results);
-    });
+user_router.get('', authentication, async (req, res) => {
+    try {
+        const users = await knex('Users').select('*');
+        return res.status(200).json({ status: 'success', data: users });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error retrieving users');
+    }
 });
 
 // get information user by id
-user_router.get('/:id', authentication, (req, res) => {
+user_router.get('/:id', authentication, async (req, res) => {
     const id = parseInt(req.params.id);
     const query = 'SELECT * FROM Users WHERE id = ?';
-    connection.query(query, [id], (error, results) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('Error retrieving user');
-        }
-        if (results.length === 0) {
+    const [user] = await connection.query(query, [id]);
+    try {
+        if (!user) {
             return res.status(404).send('User not found');
         }
-        return res.send({status: "success", data: results[0]});
-    });
+    } catch (error) {
+        return res.status(404).send('User not found');
+    }
 });
 
 // add new user
-user_router.post('', authentication, validate_name, (req, res) => {
-    const { fullname, gender, age } = req.body;
-    const query = 'INSERT INTO Users (fullname, gender, age) VALUES (?, ?, ?)';
-    connection.query(query, [fullname, gender, age], (error) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('Error inserting new item');
-        }
-        return res.status(201).send('New item inserted successfully');
-    });
+user_router.post('', authentication, validate_name, async (req, res) => {
+    const { fullname, gender, age, email, username, password } = req.body;
+    const created_at = new Date();
+    const token = req.headers['authorization'].slice(7);
+    const verify = jwt.verify(token, process.env.SECRET_KEY);
+    const created_by = verify.user_id;
+    try {
+        const user = await knex('Users').insert({
+            fullname,
+            gender,
+            age,
+            created_at,
+            created_by,
+            email,
+            username,
+            password,
+        });
+        return res.status(200).json({ status: 'success', data: user });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error deleting user');
+    }
 });
 
 // delete user by id
-user_router.delete('/:id', authentication, (req, res) => {
+user_router.delete('/:id', authentication, async (req, res) => {
     const id = parseInt(req.params.id);
     const query = 'DELETE FROM Users WHERE id = ?';
-    connection.query(query, [id], (error, results) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('Error deleting user');
-        }
-        if (results.affectedRows === 0) {
+    try {
+        const deletedUser = await knex('Users').where('id', id).del();
+        if (deletedUser === 0) {
             return res.status(404).send('User not found');
         }
         return res.status(200).send('User deleted successfully');
-    });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error deleting user');
+    }
 });
 
 // update user
-user_router.put('/:id', authentication, validate_name, (req, res) => {
+user_router.put('/:id', authentication, validate_name, async (req, res) => {
     const id = parseInt(req.params.id);
     const { fullname, gender, age } = req.body;
     const query =
         'UPDATE Users SET fullname = ?, gender = ?, age = ? WHERE id = ?';
-    connection.query(query, [fullname, gender, age, id], (error, results) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('Error updating user');
-        }
-        if (results.affectedRows === 0) {
+    try {
+        const [result] = await connection.query(query, [
+            fullname,
+            gender,
+            age,
+            id,
+        ]);
+        if (!result) {
             return res.status(404).send(`User with id ${id} not found`);
         }
-        return res.status(204).end();
-    });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error updating user');
+    }
+    return res.status(204).end();
 });
 
 // Exports cho biến user_router
