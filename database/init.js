f76d32af96554a32cf692ccdae07950905f62b7c
TuanAnh1003 <anhaanh2003@gmail.com>
2023-06-06 00:48:18 +0700

    update init database

--- database/init.js
+++ database/init.js
@@ -1,26 +1,46 @@
-const connection = require('../database/connection');
+const knex = require('../database/connection');
 require('dotenv').config();
 
 async function createTable() {
-    const createTableSql = `CREATE TABLE IF NOT EXISTS Users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fullname VARCHAR(30),
-    username VARCHAR(30),
-    password VARCHAR(500),
-    email NVARCHAR(30),
-    salt NVARCHAR(100),
-    gender INT,
-    age INT CHECK(age > 0))`;
-    await connection.query(createTableSql);
+    await knex.schema.createTableIfNotExists('Users', (table) => {
+        table.increments('id').primary();
+        table.string('fullname', 30);
+        table.string('username', 30);
+        table.string('password', 500);
+        table.string('email', 30);
+        table.string('salt', 100);
+        table.integer('gender');
+        table.integer('age').check('age', '>', 0);
+        table.timestamp('created_at').defaultTo(connection.fn.now());
+        table.integer('created_by');
+    });
 }
 
 createTable();
 
-// const query = "INSERT INTO Users (fullname, username, password, email, gender, age) VALUES ('Bui Tuan Anh', 'anh', '123','anhaanh2003@gmail.com', true, 18)";
-// connection.query(query, (err) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-//         console.log('Data inserted successfully');
-//     }
-// });
+const users = [
+    {
+        fullname: 'Sarah Williams',
+        username: 'sarahwilliams',
+        password: 'password321',
+        email: 'sarahwilliams@example.com',
+        salt: 'ghi789',
+        gender: 2,
+        age: 28,
+        created_at: new Date(),
+        created_by: null,
+    },
+    {
+        fullname: 'Robert Davis',
+        username: 'robertdavis',
+        password: 'password654',
+        email: 'robertdavis@example.com',
+        salt: 'jkl012',
+        gender: 1,
+        age: 40,
+        created_at: new Date(),
+        created_by: null,
+    },
+];
+
+await connection('Users').insert(users);
